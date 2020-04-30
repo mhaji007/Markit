fe1b070347ad75529ff6a45a81ccb740554aac7e
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-04-29 22:38:09 -0400

    Fetch products' data

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,17 @@
+import React from "react";
+import axios from "axios";
+
 function Home() {
+  React.useEffect(() => {
+    getProducts();
+  }, []);
+
+  async function getProducts() {
+    const url = "http://localhost:3000/api/products";
+    const response = await axios.get(url);
+    
+  }
+
   return <>home</>;
 }
 
