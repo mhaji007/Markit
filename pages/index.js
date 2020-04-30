7cd8eca9c07192154f657433eb2974f39ad2b29f
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-04-30 16:47:20 -0400

    Build product cards. Make components responsive

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import React from "react";
 import axios from "axios";
+import ProductList from '../components/Index/ProductList';
 
 function Home({products}) {
-  console.log(products);
+
+  return <ProductList products={products}/>
+
+
 
   // React.useEffect(() => {
   //   getProducts();
