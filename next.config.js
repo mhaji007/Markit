17190e62f765f3cb0721e84cc5890db68ce5d659
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-05-02 19:27:26 -0400

    Save product image and description to database

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     MONGO_SRV: "mongodb+srv://mhaji007:Ali23123@markit-blo6c.mongodb.net/test?retryWrites=true&w=majority",
     JWT_SECRET: "<insert-jwt-secret>",
-    CLOUDINARY_URL: "<insert-cloudinary-url>",
+    CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/dtixbjivj/image/upload",
     STRIPE_SECRET_KEY: "<insert-stripe-secret-key>"
   }
 };
--- pages/create.js
+++ pages/create.js
@@ -9,6 +9,8 @@ import {
   Header,
   Icon
 } from "semantic-ui-react";
+import axios from "axios";
+import baseUrl from "../utils/baseUrl";
 
 const INITIAL_PRODUCT = {
   name: "",
@@ -21,6 +23,7 @@ function CreateProduct() {
   const [product, setProduct] = React.useState(INITIAL_PRODUCT);
   const [mediaPreview, setMediaPreview] = React.useState("");
   const [success, setSuccess] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
 
   function handleChange(event) {
     const { name, value, files } = event.target;
@@ -32,9 +35,27 @@ function CreateProduct() {
     }
   }
 
-  function handleSubmit(event) {
+  async function handleImageUpload() {
+    const data = new FormData();
+    data.append("file", product.media);
+    data.append("upload_preset", "Markit");
+    data.append("cloud_name", "dtixbjivj");
+    const response = await axios.post(process.env.CLOUDINARY_URL, data);
+    const mediaUrl = response.data.url;
+    return mediaUrl;
+  }
+
+  async function handleSubmit(event) {
     event.preventDefault();
-    console.log(product);
+    setLoading(true);
+    const mediaUrl = await handleImageUpload();
+    console.log({ mediaUrl });
+    const url = `${baseUrl}/api/product`;
+    const { name, price, description } = product;
+    const payload = { name, price, description, mediaUrl };
+    const response = await axios.post(url, payload);
+    console.log({ response });
+    setLoading(false);
     setProduct(INITIAL_PRODUCT);
     setSuccess(true);
   }
@@ -45,7 +66,7 @@ function CreateProduct() {
         <Icon name="add" color="orange" />
         Create New Product
       </Header>
-      <Form success={success} onSubmit={handleSubmit}>
+      <Form loading={loading} success={success} onSubmit={handleSubmit}>
         <Message
           success
           icon="check"
@@ -93,6 +114,7 @@ function CreateProduct() {
         />
         <Form.Field
           control={Button}
+          disabled={loading}
           color="blue"
           icon="pencil alternate"
           content="Submit"
