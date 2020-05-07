760f2dcf0a6ab791d99f2311edf9513a9fbab568
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-05-07 16:06:20 -0400

    Implement checkout functionality

--- components/Cart/CartSummary.js
+++ components/Cart/CartSummary.js
@@ -1,8 +1,9 @@
 import React from "react";
+import StripeCheckout from "react-stripe-checkout";
 import { Button, Segment, Divider } from "semantic-ui-react";
 import calculateCartTotal from "../../utils/calculateCartTotal";
 
-function CartSummary({ products }) {
+function CartSummary({ products, handleCheckout, success }) {
   const [cartAmount, setCartAmount] = React.useState(0);
   const [stripeAmount, setStripeAmount] = React.useState(0);
   const [isCartEmpty, setCartEmpty] = React.useState(false);
@@ -19,13 +20,26 @@ function CartSummary({ products }) {
       <Divider />
       <Segment clearing size="large">
         <strong>Sub total:</strong> ${cartAmount}
-        <Button
-          icon="cart"
-          disabled={isCartEmpty}
-          color="teal"
-          floated="right"
-          content="Checkout"
-        />
+        <StripeCheckout
+          name="Markit"
+          amount={stripeAmount}
+          image={products.length > 0 ? products[0].product.mediaUrl : ""}
+          currency="USD"
+          shippingAddress={true}
+          billingAddress={true}
+          zipCode={true}
+          stripeKey="pk_test_8G5KwVt0X1wcymGC5D6w0lgc00EcqiOCPw"
+          token={handleCheckout}
+          triggerEvent="onClick"
+        >
+          <Button
+            icon="cart"
+            disabled={isCartEmpty || success}
+            color="linkedin"
+            floated="right"
+            content="Checkout"
+          />
+        </StripeCheckout>
       </Segment>
     </>
   );
--- next.config.js
+++ next.config.js
@@ -4,6 +4,6 @@ module.exports = {
     MONGO_SRV: "mongodb+srv://mhaji007:Ali23123@markit-blo6c.mongodb.net/test?retryWrites=true&w=majority",
     JWT_SECRET: "dsgi09485!fsdD1",
     CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/dtixbjivj/image/upload",
-    STRIPE_SECRET_KEY: "<insert-stripe-secret-key>"
+    STRIPE_SECRET_KEY: "sk_test_ZahFVQP0a5IjtLA6FY391aQe008cj7RP6c"
   }
 };
