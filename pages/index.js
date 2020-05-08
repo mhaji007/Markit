b63df433e8fd5f9fef52127553d7f81623eabb68
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-05-07 20:06:29 -0400

    Add pagination for product list

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import axios from "axios";
-import ProductList from '../components/Index/ProductList';
-import baseUrl from '../utils/baseUrl';
+import ProductList from "../components/Index/ProductList";
+import ProductPagination from "../components/Index/ProductPagination";
+import baseUrl from "../utils/baseUrl";
 
-function Home({products}) {
 
-  return <ProductList products={products}/>
+function Home({products, totalPages}) {
 
+  return (
+    <>
+      <ProductList products={products} />
+      <ProductPagination totalPages={totalPages} />
+    </>
+  );
 
 
   // React.useEffect(() => {
@@ -20,20 +26,20 @@ function Home({products}) {
     
   // // }
 
-  return <>home</>;
+
 }
 
-Home.getInitialProps = async () => {
+Home.getInitialProps = async ctx => {
+  const page = ctx.query.page ? ctx.query.page : "1";
+  const size = 9;
+  const url = `${baseUrl}/api/products`;
+  const payload = { params: { page, size } };
   // fetch data on server
-    const url = `${baseUrl}/api/products`;
-    const response = await axios.get(url);
-    return {products: response.data};
-    
-  
+  const response = await axios.get(url, payload);
   // return response data as an object
-  // this object will be merged with existing props
-}
-
-
+  return response.data;
+  // note: this object will be merged with existing props
+};
 
 export default Home;
+
