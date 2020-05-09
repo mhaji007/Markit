64ae5b29b836051ecd505d33ab30af99009ea4da
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-05-09 13:33:58 -0400

    Sort products and users

--- pages/api/users.js
+++ pages/api/users.js
@@ -7,7 +7,8 @@ export default async (req, res) => {
       req.headers.authorization,
       process.env.JWT_SECRET
     );
-    const users = await User.find({ _id: { $ne: userId } });
+    const users = await User.find({ _id: { $ne: userId } }).
+    sort({role: "asc"});
     res.status(200).json(users);
   } catch (error) {
     console.error(error);
