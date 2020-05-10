371a14ec10dad6d127ec4811fac08c0005e6ebfc
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-05-10 19:49:46 -0400

    Markit app on Heroku

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   env: {
     MONGO_SRV: "mongodb+srv://mhaji007:Ali23123@markit-blo6c.mongodb.net/test?retryWrites=true&w=majority",
-    JWT_SECRET: "dsgi09485!fsdD1",
+    JWT_SECRET: "dsgi09485ffsdD1",
     CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/dtixbjivj/image/upload",
     STRIPE_SECRET_KEY: "sk_test_ZahFVQP0a5IjtLA6FY391aQe008cj7RP6c"
   }
